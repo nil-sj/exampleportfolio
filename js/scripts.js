cb85c1f825bcc307a6e101226e4824a98c0c7466
Nil SJ <niladri123@gmail.com>
2022-03-20 20:13:42 -0400

    add scripts for hamburger button

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
 //alert("Hello world");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach(link => {
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  });
+});
